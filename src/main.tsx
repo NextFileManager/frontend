f38bea8313af61313c26ada4c9e5c4fb4b631436
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2025-01-17 12:46:43 -0600

    fixed frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-}
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
